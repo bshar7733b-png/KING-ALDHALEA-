0a1615b53aa626ed39854a6b8cb09c39cc0e16ba
bshar7733b-png <bshar.7733b@gmail.com>
2026-09-24 13:40:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '967730909379', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
